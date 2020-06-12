069e410d93ec82ae221a72c05d77791ac4c6251f
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-06-12 21:38:52 +0430

    Fix hamburgur menu position and Modify some icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(window).on("load", () => {
   $(".loader").hide();
 });
+
+$("#check-s").click(() => {
+  if ($("#check-s").is(":checked") && $("#check").is(":checked")) {
+    $("#check").prop("checked", false);
+  }
+});
+
+$("#check").click(() => {
+  if ($("#check").is(":checked") && $("#check-s").is(":checked")) {
+    $("#check-s").prop("checked", false);
+  }
+});
